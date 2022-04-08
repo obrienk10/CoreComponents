77763f74bca806a61f211f7a93642d8d8179b1cc
Karl.OBrien <karl.obrien@leaseplan.com>
2022-04-08 22:35:23 +0200

    add Material UI

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Button.scss";
-
+import MUIButton from '@mui/material/Button';
 export interface ButtonProps {
     label: string;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>;
+    return <MUIButton variant="text">{props.label}</MUIButton>;
 };
 
 export default Button;
