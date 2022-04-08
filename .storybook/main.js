aa41b941e64738e8544c8f4acdcce928cc002dcb
Karl.OBrien <karl.obrien@leaseplan.com>
2022-04-08 21:55:25 +0200

    add storybook & testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,10 +6,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/preset-scss"
   ],
-  "framework": "@storybook/react",
   "core": {
-    "builder": "@storybook/builder-webpack5"
+    "builder": "webpack5"
   }
 }
